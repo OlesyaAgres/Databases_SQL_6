c5f2ec6a528ee66093799637590ae6b54e6de10c
Olesya <olesyaspb.ru@mail.ru>
2023-10-26 20:19:57 +0300

    решение 3

--- _vk_db_task.sql
+++ _vk_db_task.sql
@@ -132,3 +132,18 @@ CALL DeleteUserProc(2);
 -- 3. * Написать триггер, который проверяет новое появляющееся сообщество. 
 -- Длина названия сообщества (поле name) должна быть не менее 5 символов. 
 -- Если требование не выполнено, то выбрасывать исключение с пояснением.
+
+DROP TRIGGER IF EXISTS VerificationCommunityNameTrigger;
+
+DELIMITER //
+
+CREATE TRIGGER VerificationCommunityNameTrigger BEFORE INSERT ON `vk`.`Communities` 
+FOR EACH ROW BEGIN
+   IF (LENGTH(new.name) < 5) THEN
+       SIGNAL SQLSTATE '45000'
+	   SET MESSAGE_TEXT = 'Длина названия сообщества (поле name) должна быть не менее 5 символов';
+       INSERT INTO VerificationCommunityNameTrigger_exception_table VALUES();
+   END IF; 
+END; // 
+
+DELIMITER ;
