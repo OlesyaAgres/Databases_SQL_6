b9c17c9686a8b3fe358521b58a4ed84e3bda46b9
Olesya <olesyaspb.ru@mail.ru>
2023-10-26 20:19:01 +0300

    решение 2

--- _vk_db_task.sql
+++ _vk_db_task.sql
@@ -63,6 +63,72 @@ SELECT DeleteUserFunc(1) AS user_id_del;
 -- 2. Предыдущую задачу решить с помощью процедуры и обернуть используемые команды в 
 -- транзакцию внутри процедуры.
 
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+DROP PROCEDURE IF EXISTS DeleteUserProc;
+
+DELIMITER //
+
+CREATE PROCEDURE DeleteUserProc (user_id_del INT)
+BEGIN
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    
+    BEGIN
+        ROLLBACK;
+    END;
+
+	START TRANSACTION;
+    
+		DELETE 
+        FROM `vk`.`likes` l
+		WHERE l.user_id = user_id_del;
+    
+		DELETE 
+        FROM `vk`.`users_communities` uc
+		WHERE uc.user_id = user_id_del;
+    
+		DELETE 
+        FROM `vk`.`messages` m
+		WHERE m.to_user_id = user_id_del 
+            OR m.from_user_id = user_id_del;
+    
+		DELETE 
+        FROM `vk`.`friend_requests` fr
+		WHERE fr.initiator_user_id = user_id_del 
+            OR fr.target_user_id = user_id_del;
+    
+		DELETE l
+		FROM `vk`.`media` m
+		JOIN `vk`.`likes` l ON l.media_id = m.id
+		WHERE m.user_id = user_id_del;
+    
+		UPDATE `vk`.`profiles` p
+		JOIN `vk`.`media` m ON p.photo_id = m.id
+		SET p.photo_id = NULL
+		WHERE m.user_id = user_id_del;
+
+		DELETE 
+        FROM `vk`.`media` m
+		WHERE m.user_id = user_id_del;
+    
+		DELETE 
+        FROM `vk`.`profiles` p
+		WHERE p.user_id = user_id_del;
+    
+		DELETE 
+        FROM `vk`.`users` u
+		WHERE u.id = user_id_del;
+         
+	COMMIT;
+
+END; // 
+
+DELIMITER ;
+
+CALL DeleteUserProc(2);
+
+
 -- 3. * Написать триггер, который проверяет новое появляющееся сообщество. 
 -- Длина названия сообщества (поле name) должна быть не менее 5 символов. 
 -- Если требование не выполнено, то выбрасывать исключение с пояснением.
